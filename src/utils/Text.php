2870fae7fbb9c46f2c6fdc01d1a7958f088ef332
Administrator <fwg199016>
2022-01-05 17:43:38 +0800

    x

--- src/utils/Text.php
+++ src/utils/Text.php
@@ -26,7 +26,7 @@ class Text
     　　* step1.原字符串转小写,原字符串中的分隔符用空格替换,在字符串开头加上分隔符
     　　* step2.将字符串中每个单词的首字母转换为大写,再去空格,去字符串首部附加的分隔符.
     　　*/
-    function camelize($uncamelized_words,$separator='_'): string
+    public static function camelize($uncamelized_words,$separator='_'): string
     {
         $uncamelized_words = $separator. str_replace($separator, " ", strtolower($uncamelized_words));
         return ltrim(str_replace(" ", "", ucwords($uncamelized_words)), $separator );
@@ -37,7 +37,7 @@ class Text
 　　* 思路:
 　　* 小写和大写紧挨一起的地方,加上分隔符,然后全部转小写
 　　*/
-    function uncamelize($camelCaps,$separator='_'): string
+    public static function uncamelize($camelCaps,$separator='_'): string
     {
         return strtolower(preg_replace('/([a-z])([A-Z])/', "$1" . $separator . "$2", $camelCaps));
     }
